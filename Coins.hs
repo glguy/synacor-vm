dbf93e58df4fc39b07714c98637800fe96e207a4
Eric Mertens <emertens@gmail.com>
2015-12-14 09:11:50 -0800

    cleanups

--- Coins.hs
+++ Coins.hs
@@ -2,6 +2,14 @@ module Main where
 
 import Data.List
 
+coins :: [Int]
 coins = [2,3,5,7,9]
+
+check :: [Int] -> Bool
 check [a,b,c,d,e] = a + b * c^2 + d^3 - e == 399
-main = print (find check (permutations coins))
+
+solutions :: [[Int]]
+solutions = filter check (permutations coins)
+
+main :: IO ()
+main = mapM_ print solutions
